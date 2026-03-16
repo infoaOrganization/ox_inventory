a7bb77fce9f67fc97201e70b5f8d17e6e710753c
UnityTrabler <kdhmm0325@naver.com>
2026-03-16 14:04:54 +0900

    chore: change binding keys

--- init.lua
+++ init.lua
@@ -79,7 +79,7 @@ else
     client = {
         autoreload = GetConvarInt('inventory:autoreload', 0) == 1,
         screenblur = GetConvarInt('inventory:screenblur', 1) == 1,
-        keys = json.decode(GetConvar('inventory:keys', '')) or { 'F2', 'K', 'TAB' },
+        keys = json.decode(GetConvar('inventory:keys', '')) or { 'TAB', 'HOME', '`' },
         enablekeys = json.decode(GetConvar('inventory:enablekeys', '[249]')),
         aimedfiring = GetConvarInt('inventory:aimedfiring', 0) == 1,
         giveplayerlist = GetConvarInt('inventory:giveplayerlist', 0) == 1,
